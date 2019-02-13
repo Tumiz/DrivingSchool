924bf0acf4c3fcd0ce1ec0968b6f3a90b0ac224c
huangjiajun <hh11698@163.com>
2019-02-13 17:52:53 +0800

    not work

--- ai.py
+++ ai.py
@@ -7,57 +7,22 @@ import torch.optim as optim
 import visdom
 
 class AI:
-    def __init__(self):
-        self.planner = Planner()
-        self.controller = Controller()
-        self.destination = 20
-
-    def decision(self, x_gap, v):
-        v_target = self.planner.decision(x_gap)
-        a = self.controller.decision(v-v_target)
-        return a
-
-
-class Planner(nn.Module):
 
     def __init__(self):
-        super(Planner, self).__init__()
-        self.layer1 = nn.Linear(1, 20)
-        self.layer2 = nn.Linear(20, 20)
-        self.layer3 = nn.Linear(20, 1)
-        self.x_gap = 0.
-        self.v_target = 0.  # target velocity
-        self.experience = []
-        self.loss = torch.tensor([1.])
+        self.actor = Actor()
+        self.critic = Critic()
+        self.x_gap=0
+        self.v=0
+        self.a=0
+
         self.viz=visdom.Visdom()
         self.plot_velocity=self.viz.line(X=[0],Y=[0])
         self.v_history=[]
         self.t_history=[]
 
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        x = self.layer3(x)
-        return x
-
-    def q(self, x_gap):  # return Q
-        return self(torch.tensor([x_gap])).item()
-
-    def update_experience(self,x_gap):
-        R=abs(self.x_gap)-abs(x_gap)
-        for i,e in enumerate(self.experience):
-            if abs(e[0]-self.x_gap)<0.01 and e[2]<R:
-                del self.experience[i]
-        self.experience.append([self.x_gap, self.v_target, R])
-
-    def decision(self, x_gap, greedy, v, t):  # return A
-        if(abs(x_gap)< abs(self.x_gap)):
-            self.update_experience(x_gap)
-        self.v_target = self.q(x_gap)+random.gauss(0,greedy)
-        self.v_target=min(3,self.v_target)
-        self.v_target=max(-1,self.v_target)
-        self.x_gap = x_gap
-        self.train()
+    def decision(self, x_gap, v, R, t, greedy):  # return A
+        Q=self.critic.value()
+        self.actor.update()
 
         self.v_history.append(v)
         self.t_history.append(t)
@@ -67,39 +32,52 @@ class Planner(nn.Module):
         self.viz.line(X=self.t_history,Y=self.v_history,win=self.plot_velocity)
         return self.v_target
 
-    def train(self):
-        l = len(self.experience)
-        if(l == 0):
-            return
-        if(l > 1000):
-            self.experience=random.sample(self.experience, 1000)
-        sample = torch.tensor(self.experience)
-        loss_func = nn.MSELoss(reduction='mean')
-        optimizer = optim.SGD(self.parameters(), lr=0.01)
-        input = sample[:, 0:1].float()
-        target = sample[:, 1:2].float()
-        counts = 0
-        self.loss = loss_func(self(input), target)
-        while not torch.isnan(self.loss) and counts < 10:
-            loss = loss_func(self(input), target)
-            self.loss = loss
-            optimizer.zero_grad()   # zero the gradient buffers
-            loss.backward()
-            optimizer.step()
-            counts += 1
-
-
-class Controller(nn.Module):
-
+class Actor(Net):
     def __init__(self):
-        super(Controller, self).__init__()
-        self.layer1 = nn.Linear(1, 20)
-        self.layer2 = nn.Linear(20, 20)
-        self.layer3 = nn.Linear(20, 1)
-        self.v_gap = 0.  # gap between real velocity and target velocity
-        self.a = 0.  # acc
+        super(Actor, self).__init__(2, 1, 1000)
+        self.x_gap=0
+        self.v=0
+        self.a=0
+        
+    def value(self,x_gap,v):
+        return self(torch.tensor([x_gap,v])).item()
+
+    def update(self,Q):
+        for i,e in enumerate(self.experience):
+            if pow(e[0]-self.x_gap)+pow(e[1]-self.v)+pow(e[2]-self.a)<0.0001 and e[3]<Q:
+                del self.experience[i]
+        self.experience.append([self.x_gap, self.v, self.a, Q])
+        self.train()
+
+    def decision(self,x_gap,v,greedy):
+        self.a=self.value(x_gap,v)+random.gauss(0,greedy)
+        self.a=min(1,self.a)
+        self.a=max(-1,self.a)
+        self.x_gap=x_gap
+        self.v=v
+
+class Critic(Net):
+    def __init__(self):
+        super(Critic, self).__init__(3, 1, 1000)
+
+    def value(self,x_gap,v,a):
+        return self(torch.tensor([x_gap,v,a])).item()
+
+    def update(self,R):
+
+
+class Net(nn.Module):
+
+    def __init__(self,input_number,output_number,experience_pool_size):
+        super(Net,self).__init__()
+        self.layer1 = nn.Linear(input_number, input_number*10)
+        self.layer2 = nn.Linear(input_number*10, output_number*10)
+        self.layer3 = nn.Linear(output_number*10, output_number)
         self.experience = []
         self.loss = torch.tensor([1.])
+        self.input_number=input_number
+        self.output_number=output_number
+        self.experience_pool_size=experience_pool_size
 
     def forward(self, x):
         x = F.relu(self.layer1(x))
@@ -107,40 +85,23 @@ class Controller(nn.Module):
         x = self.layer3(x)
         return x
 
-    def reached(self, v_gap):
-        return v_gap < 0.01 and self.a < 0.01
-
-    def q(self, v):  # return Q
-        return self(torch.tensor([v])).item()
-
-    def decision(self, v_gap):  # input gap to target velocity, output acc
-        if abs(v_gap) < abs(self.v_gap):
-            self.experience.append([self.v_gap, self.a])      
-            self.a = self.q(v_gap)
-        else:
-            self.a = random.uniform(-1, 1)
-        self.v_gap = v_gap
-        self.train()
-        print(v_gap,self.a,)
-        return self.a
-
     def train(self):
         l = len(self.experience)
         if(l == 0):
             return
-        if(l > 1000):
-            self.experience=random.sample(self.experience, 1000)
+        if(l > self.experience_pool_size):
+            self.experience=random.sample(self.experience, self.experience_pool_size)
         sample = torch.tensor(self.experience)
         loss_func = nn.MSELoss(reduction='mean')
         optimizer = optim.SGD(self.parameters(), lr=0.01)
-        input = sample[:, 0:1].float()
-        target = sample[:, 1:2].float()
+        input = sample[:, 0:self.input_number].float()
+        target = sample[:, self.input_number:self.input_number+self.output_number].float()
         counts = 0
         self.loss = loss_func(self(input), target)
         while not torch.isnan(self.loss) and counts < 10:
             loss = loss_func(self(input), target)
             self.loss = loss
-            optimizer.zero_grad()   # zero the gradient buffers
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             counts += 1
