b63630dd6cce154b3e55cbc66dbc12e405e22226
Tumiz <hh11698@163.com>
2019-04-02 00:04:54 +0800

    .

--- algris.py
+++ algris.py
@@ -1,14 +1,19 @@
 from torch import tensor
+from visdom import Visdom
+from torch import tensor, arange
+viz=Visdom()
+
 def normalize(data):# input tensor
     ret = (data-data.mean())/(data.std()+1e-10)
     return ret
 
 def gather(feeds,gamma):#input list
     values=[]
-    feedsum=0
     V=0
     for feed in feeds[::-1]:
         V=feed+gamma*V
         values.insert(0,V)
-        feedsum+=feed
-    return values, feedsum
+    return values
+
+def plot(x,y):
+    viz.line(X=x,Y=y)
