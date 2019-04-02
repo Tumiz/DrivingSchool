7ef97b3e41e1eaea5d59115fa2792482fc025b38
huangjiajun <huangjiajun@holomatic.com>
2019-04-02 18:47:03 +0800

    1

--- agents/policygradientcontinuous.py
+++ agents/policygradientcontinuous.py
@@ -1,12 +1,28 @@
 from torch.optim import Adam
-from torch import tensor, arange, stack, isnan, tanh
+from torch import tensor, arange, stack, isnan, tanh, from_numpy
 from torch.nn import Module, Linear
 from torch.nn.functional import softplus, elu
 from torch.distributions.normal import Normal
 from algris import normalize, gather
+from itertools import count
+from numpy import array
+import argparse
+import gym
 
-import visdom
+parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 543)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+args = parser.parse_args()
 
+
+env = gym.make('MountainCarContinuous-v0')
+env.seed(args.seed)
 
 class Policy(Module):
     def __init__(self):
@@ -16,11 +32,11 @@ class Policy(Module):
         self.a_head = Linear(2*16, 2)
 
     def forward(self, x):
+        print("-------X",x)
         x1 = elu(self.s_head(x))
         return self.a_head(x1)
         
-
-
+state_=[]
 class Agent(Module):
 
     def __init__(self):
@@ -28,39 +44,25 @@ class Agent(Module):
         self.policy = Policy()
         self.optimizer = Adam(self.parameters(), lr=0.01)
         self.feeds = []  # records of returns
-        self.state_actions = []
         self.a_logprobs = []
-        self.w_logprobs = []
-
-        self.viz = visdom.Visdom()
-        self.viz.close(win=None)
-        self.plot_v = self.viz.line(X=[0], Y=[0])
-        self.plot_a = self.viz.line(X=[0], Y=[0])
-        self.plot_w = self.viz.line(X=[0], Y=[0])
-        self.plot_r = self.viz.line(X=[0], Y=[0])
-        self.plot_l = self.viz.line(X=[0], Y=[0])
-        self.plot_value = self.viz.line(X=[0], Y=[0])
-        self.v_history = []
-        self.w_history = []
-        self.a_history = []
-        self.f_history = []
-        self.l_history = []
 
     def select_action(self,state):
-        a_mu,a_sigma=self.policy(state)
-        self.state_actions.append([state,a_mu,a_sigma])
+        global state_
+        state_=tensor(state)
+        a_mu,a_sigma=self.policy(tensor(state))
         a_sigma = softplus(a_sigma)
         a_dist = Normal(a_mu, a_sigma)
         a = a_dist.sample()
         a_logprob = a_dist.log_prob(a)
-        return a,a_logprob
+        self.a_logprobs.append(a_logprob)
+        return a.numpy()
 
     def finish_episode(self):
         values = gather(self.feeds, 0.99)
         tvalues = tensor(values)
         nvalues = normalize(tvalues)
         losses = []
-        for nvalue, a_logprob in zip(nvalues, self.a_logprobs, self.w_logprobs):
+        for nvalue, a_logprob in zip(nvalues, self.a_logprobs):
             # log(1/p(a,w))=-log(p(a)*p(w))=-log(p(a))-log(p(w))
             losses.append(nvalue*(-a_logprob-w_logprob))
         loss = stack(losses).sum()
@@ -70,8 +72,6 @@ class Agent(Module):
 
         self.feeds = []
         self.a_logprobs = []
-        self.w_logprobs = []
-        self.states = []
         
 agent=Agent()
 running_reward = 10
@@ -79,6 +79,7 @@ for i_episode in count(1):
     state, ep_reward = env.reset(), 0
     for t in range(1, 10000):  # Don't infinite loop while learning
         action = agent.select_action(state)
+        print(action)
         state, reward, done, _ = env.step(action)
         if args.render:
             env.render()
